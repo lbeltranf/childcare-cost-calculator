d43be7b8d9f09f9fd9de1715bc3d334660968f0b
lbeltranf <beltran@pennpolicy.org>
2025-07-01 14:36:32 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -19,11 +19,36 @@ st.set_page_config(page_title="Public Policy Cost Dashboard", layout="wide")
 st.markdown("""
 <style>
     html, body, [data-testid="stAppViewContainer"] {
-        height: 100%;
-        overflow: hidden !important;
-        transform: scale(0.8);
-        transform-origin: top left;
-        width: 125%;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden !important;
+    transform: scale(0.8);
+    transform-origin: top left;
+    padding: 0;
+    margin: 0;
+}
+    @supports (-webkit-touch-callout: none) {
+        html, body, [data-testid="stAppViewContainer"] {
+            transform: scale(0.8);
+            transform-origin: top left;
+            width: 125%;
+        }
+    }
+    @supports not (-webkit-touch-callout: none) {
+        html, body, [data-testid="stAppViewContainer"] {
+            zoom: 80%;
+        }
+    }
+    @media not all and (min-resolution:.001dpcm) {
+        @supports (-webkit-appearance:none) {
+            html, body, [data-testid="stAppViewContainer"] {
+                transform: scale(0.8);
+                transform-origin: top left;
+                width: 125%;
+            }
+        }
+    }}
+        zoom: 80%;
     }
     header > div:first-child, .stDeployButton, .st-emotion-cache-1wbqy5l {
         display: none !important;
