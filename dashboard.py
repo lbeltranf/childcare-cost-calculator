32ea85c3d5256db87dfdcd036cc05905b72b9082
lbeltranf <beltran@pennpolicy.org>
2025-07-01 12:54:26 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -21,7 +21,18 @@ st.markdown("""
     html, body, [data-testid="stAppViewContainer"] {
         height: 100%;
         overflow: hidden !important;
-        zoom: 80%;
+    }
+    @supports (-webkit-touch-callout: none) {
+        html, body, [data-testid="stAppViewContainer"] {
+            transform: scale(0.8);
+            transform-origin: top left;
+            width: 125%;
+        }
+    }
+    @supports not (-webkit-touch-callout: none) {
+        html, body, [data-testid="stAppViewContainer"] {
+            zoom: 80%;
+        }
     }
     @media not all and (min-resolution:.001dpcm) {
         @supports (-webkit-appearance:none) {
